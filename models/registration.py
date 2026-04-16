5fe40ca65df9e0bcc07a237ebfaec652b41ad968
adityaraj-4u <adityaraj47372@gmail.com>
2026-04-16 07:45:21 +0530

    registration.py

--- models/registration.py
+++ models/registration.py
@@ -4,7 +4,7 @@ class Registration:
     def __init__(self, student_id, course_code, status="Pending"):
         self.student_id = student_id
         self.course_code = course_code
-        self.status = status  # Can be: Pending, Approved, Rejected
+        self.status = status  
 
     def __str__(self):
         return f"Student: {self.student_id} | Course: {self.course_code} | Status: {self.status}"
