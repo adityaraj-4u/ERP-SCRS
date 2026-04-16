ddfddfad07f7872d62454bcd6501be1076c4afaa
adityaraj-4u <adityaraj47372@gmail.com>
2026-04-16 07:45:02 +0530

    course.py

--- models/course.py
+++ models/course.py
@@ -4,14 +4,14 @@ class Course:
         self.name = name
         self.credits = credits
         self.seats = seats
-        self.schedule = schedule  # Tuple: ("MON", "09:00")
+        self.schedule = schedule  
         self.prerequisites = prerequisites
-        self.faculty = faculty    # ✅ Added Faculty
+        self.faculty = faculty    
         self.enrolled = 0   
 
     def seats_available(self):
         return self.seats - self.enrolled
 
     def __str__(self):
-        # ✅ Now prints Faculty and Schedule on the main menu
+        
         return f"[{self.code}] {self.name} | Prof: {self.faculty} | Schedule: {self.schedule[0]} at {self.schedule[1]} | ({self.seats_available()} seats left)"
